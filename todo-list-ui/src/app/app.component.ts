4ee71b914a2704f25f9b8711a4f4b04aa30bb69a
Sega1A <sergio.garciacuchallo@gmail.com>
2024-11-24 11:12:04 -0400

    feature: Todo list view.

--- todo-list-ui/src/app/app.component.ts
+++ todo-list-ui/src/app/app.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { HelloWorld } from './HelloWorld/helloWorld.component';
+import { Todo } from './Todo/todo.component';
+import { todos } from '../mocks/mocks.todos';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HelloWorld],
+  imports: [CommonModule, HelloWorld, Todo],
   templateUrl: './app.component.html',
 })
 export class AppComponent {
-  title = "todo-list-ui";
+  title = 'todo-list-ui';
+  todos = todos;
 }
